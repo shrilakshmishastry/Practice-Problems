6ed5b111435854095c563cdaeae0d09eee26b745
shrilakshmishastry <shrilakshmishastry@gmail.com>
2019-12-06 21:31:11 +0530

    Crc

--- crc.java
+++ crc.java
@@ -1,72 +1,64 @@
-// program to check crc
 import java.util.*;
 
-
 class crc{
-  public static String csum;
-  public static String genep="10001000000100001";
   public static String msg;
-  public static int totalLen;
+  public static String genp = "10001000000100001";
+  public static String csum;
+  public static int totallen;
   public static int genplen;
-  public  static int msgLen;
-  public static String xor(String csum, String genep ){
-    int csumI = Integer.parseInt(csum,2);
-    int genepI = Integer.parseInt(genep,2);
-    int result = csumI ^ genepI;
-    return  Integer.toBinaryString(result);
+  public static int msglen;
+  public static String xor(String csum,String genp){
+    int gen = Integer.parseInt(genp,2);
+    int csum1 = Integer.parseInt(csum,2);
+    int result = gen^csum1;
+    return Integer.toBinaryString(result);
   }
-  public static void crc(){
-    System.out.println("hello");
-    int i = genep.length();
+  public static String crcgen(){
+    int i = genplen;
     csum = msg.substring(0,genplen);
-    do{
+    do {
       if(csum.charAt(0)!='1'){
-        csum =csum.substring(1,csum.length());
+        csum = csum.substring(1,csum.length());
       }
-      if(csum.length()== genep.length()){
-      csum =  xor(csum,genep);
+      if(csum.length()==genplen){
+        csum = xor(csum,genp);
       }
-      csum +=msg.charAt(i);
-      i++;
-    }while(i<totalLen);
-    if(csum.length()== genep.length()){
-    csum=  xor(csum,genep);
+      csum = csum+msg.charAt(i++);
+    } while (i<totallen);
+    if(csum.length()==genplen){
+      csum = xor(csum,genp);
     }
+    return csum;
   }
   public static void main(String[] args) {
-    System.out.println("hello");
-    System.out.println("result= "+xor("10001","10001"));
-    System.out.println("enter the message");
     Scanner s = new Scanner(System.in);
-    msg = s.next();
-    System.out.println("entered message"+msg);
-    msgLen = msg.length();
-    System.out.println("entered message length"+msgLen);
-    genplen = genep.length();
-    totalLen = msgLen+genplen-1;
-    for (int i=msgLen;i<totalLen;i++){
-      msg+="0";
+    System.out.println("enter the data");
+    msg = s.nextLine();
+    System.out.println("generating polynomial is"+genp);
+    msglen = msg.length();
+    genplen = genp.length();
+    totallen = msglen+genplen-1;
+    for(int i = msglen;i<totallen;i++){
+      msg+= 0;
     }
-    crc();
+    System.out.println("msg after modification"+msg);
+    crcgen();
     msg = xor(msg,csum);
-    System.out.println("entered message"+msg);
-    System.out.println("csum"+csum);
-    // System.out.println("csum"+csum);\
-    int length = msg.length();
-    System.out.println("enter message");
-    msg = s.next();
-    System.out.println("entered message"+msg);
-    crc();
-    if(length!=msg.length()){
-      System.out.println("erroe");
-    }
-
-    if(csum.contains("1")){
+    System.out.println("generated message"+msg);
+    int newLen = msg.length();
+    System.out.println("enter receive data");
+    msg = s.nextLine();
+    if(msg.length()!= newLen){
       System.out.println("error");
     }
     else{
-      System.out.println("no error");
+      crcgen();
+      if(csum.contains("1")){
+        System.out.println("error");
+      }
+      else{
+        System.out.println("no error  ");
+      }
     }
-
   }
 }
