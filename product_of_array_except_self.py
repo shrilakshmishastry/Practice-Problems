d86263139f56053b27e10d3f1fb8bbb0002dcab1
shrilakshmishastry <shrilakshmishastry@gmail.com>
2020-04-16 11:41:12 +0530

    Program to find product of list except self

--- product_of_array_except_self.py
+++ product_of_array_except_self.py
@@ -1,4 +1,6 @@
-# import numpy as np
+# Given an array nums of n integers where n > 1,  
+# return an array output such that output[i]
+# is equal to the product of all the elements of nums except nums[i].
 
 l = map(int,input().strip().split())
 l = list(l)
