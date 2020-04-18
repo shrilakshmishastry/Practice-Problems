eb1eb51fa30b979e88956e2ee0e0c959585f3bba
shrilakshmishastry <shrilakshmishastry@gmail.com>
2020-04-18 18:38:30 +0530

    Program to count number of islands using bfs

--- numbers_of_islands.py
+++ numbers_of_islands.py
@@ -2,14 +2,14 @@
 # count the number of islands. An island is surrounded by water and
 #  is formed by connecting adjacent lands horizontally or vertically.
 # You may assume all four edges of the grid are all surrounded by water.
-def dfs(i,j)            :
+def bfs(i,j)            :
     if (i<0 or j<0 or i>=len(a) or j>=len(a[i]) or a[i][j] == 0):
         return ;
     a[i][j]=0
-    dfs(i+1,j)
-    dfs(i-1,j)
-    dfs(i,j+1)
-    dfs(i,j-1)
+    bfs(i+1,j)
+    bfs(i-1,j)
+    bfs(i,j+1)
+    bfs(i,j-1)
 
 n = int(input("enter the size of row"))
 m = int(input("enter the size of column"))
@@ -27,5 +27,5 @@ for i in range(0,n):
     for j in range(0,m):
         if a[i][j] == 1:
             count = count+1
-            dfs(i,j)
+            bfs(i,j)
 print(count)
